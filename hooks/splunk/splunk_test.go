b2de6ef06a45193d71796d76795284c8c1736019
Edward Tsang <etsang@splunk.com>
2017-01-04 15:52:49 -0800

    add some skeleton test

--- hooks/splunk/splunk_test.go
+++ hooks/splunk/splunk_test.go
@@ -5,3 +5,39 @@ import "testing"
 func TestInitHook(t *testing.T) {
 
 }
+
+func TestTearDownHook(t *testing.T) {
+
+}
+
+func TestSendBatchHook(t *testing.T) {
+
+}
+
+func TestSendMessageSizeLessThanBatchHook(t *testing.T) {
+
+}
+
+func TestSendMessageSizeLargerThanBatchHook(t *testing.T) {
+
+}
+
+func TestSendMessageFlushHook(t *testing.T) {
+
+}
+
+func TestSendSplunkMessageHook(t *testing.T) {
+
+}
+
+func TestBadCredentialHook(t *testing.T) {
+
+}
+
+func TestBadUrlHook(t *testing.T) {
+
+}
+
+func TestBadPortHook(t *testing.T) {
+
+}
