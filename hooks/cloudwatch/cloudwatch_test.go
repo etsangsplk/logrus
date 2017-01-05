dab18bad791b640868db18544fb80f47e6887a66
Edward Tsang <etsang@splunk.com>
2017-01-04 16:10:19 -0800

    Add some test skeleton

--- hooks/cloudwatch/cloudwatch_test.go
+++ hooks/cloudwatch/cloudwatch_test.go
@@ -5,3 +5,31 @@ import "testing"
 func TestInitHook(t *testing.T) {
 
 }
+
+func TestTearDownHook(t *testing.T) {
+
+}
+
+func TestBadCredentialHook(t *testing.T) {
+
+}
+
+func TestBadUrlHook(t *testing.T) {
+
+}
+
+func TestPutAlarm(t *testing.T) {
+
+}
+
+func TestPutAlarmWithAction(t *testing.T) {
+
+}
+
+func TestPutAlarmInvalidComapirsonOperator(t *testing.T) {
+
+}
+
+func TestPutAlarmInvalidStatistic(t *testing.T) {
+
+}
